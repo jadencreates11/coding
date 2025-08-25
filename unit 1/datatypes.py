0b3ca6d2f7f60a4d454ed213964d59ffe2be37da
jadencreates11 <jhart184@boyslatin.net>
2025-08-25 13:14:36 +0000

    class work

--- unit 1/datatypes.py
+++ unit 1/datatypes.py
@@ -1,7 +1,34 @@
 #strings- anything wrapped in quotes
+# quoates sting the data together
 "Jaden Hart"
 "Good Morning, have a good day."
 "123"
 #Intergers- whole numbers.
 12
 13
+#comments - invisible text that only
+# humans can read and see
+#  comments are created with the hashtag symbol (#)
+# Integers - whole numbers
+10
+120
+34000
+# Floats - decimal numbers
+0.1
+20.03
+30.234
+10.0
+10.0000001232
+# Boolean - values that represnt true or false
+true
+false
+1
+0
+
+""
+"abc"
+# Variable - a cintainer for data
+# to create a variable we first give it a name,
+#then we assign it a value
+
+SchoolName=boyslatin
