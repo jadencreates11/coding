c8807212016ffd65a681192a737ffe44be7b8689
jadencreates11 <jhart184@boyslatin.net>
2025-08-27 13:37:21 +0000

    class work

---
+++ unit 1/operaters.py
@@ -0,0 +1,17 @@
+# Operater
+# operaters are a construct (bulit in system) that give
+# data types more actions and power.
+
+# Arithmetic Operater family
+# Arithmetic operaters are simply math operaters.
+
+# print is anything inside ()
+print (2+3)
+print ( 20 -100)
+print ( 40 /  2)
+print ( 30.9 * 10)
+
+schoolName = "Boya Latin HIgh"
+age = 16
+age += 1.5
+print(age)
