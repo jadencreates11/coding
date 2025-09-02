05f7fcc6e8f05b36c75ea79b2979912f04a52ed8
jadencreates11 <jhart184@boyslatin.net>
2025-09-02 13:37:26 +0000

    work

---
+++ builtinfunctions.py
@@ -0,0 +1,10 @@
+# built in functions - prewritten code that is stored
+# in the programming language. so long as you know the
+# keyboard you can use it's power
+
+#float() function - allows us to transform a whole number
+#into a decimal number.
+
+transfer = 50
+bank = 12.00
+print(float(transfer) + bank)
