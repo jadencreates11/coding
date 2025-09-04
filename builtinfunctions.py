04aa9b56804f4140cd17c931383e9adcda4b534a
jadencreates11 <jhart184@boyslatin.net>
2025-09-04 13:37:33 +0000

    built in activity

--- builtinfunctions.py
+++ builtinfunctions.py
@@ -11,3 +11,16 @@ print(float(transfer) + bank)
 time = input('Good morning')
 print('the current time is'+ time)
 print
+# string - change data type into string
+# int - change data type into whole number
+# float - change data type into decimal number
+name = Jaden ("what is your name")
+age = 17 ("what is your age")
+#sring we didny have to change anything
+Bonus = 200
+print(300 +Bonus)
+#integer remove the "" from 200
+
+Value = "true"
+print(True==Value)
+# Chane this into a Bool and Make the T uppercase
