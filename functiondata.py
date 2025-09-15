f79771a5754251fc7a6a3ecb2c3677adf685d75e
jadencreates11 <jhart184@boyslatin.net>
2025-09-15 13:18:01 +0000

    work

---
+++ functiondata.py
@@ -0,0 +1,16 @@
+
+# How create a function with data
+
+# step 1 function dewfinition
+def bnbReFund(accountNumber, refundAmount):
+    print("account number:" +str(refundAmount))
+    print('refund amount: $' +str(refundAmount))
+
+# step 2. function call(invocation)
+bnbReFund(19292992, 3000) # argumaents are real data
+
+Name= "Jaden Hart"
+Birthday="December 9th 2007"
+
+
+
