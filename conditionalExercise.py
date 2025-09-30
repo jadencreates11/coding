d5360334a9275b87acfaf305c15c8f479039a3f1
jadencreates11 <jhart184@boyslatin.net>
2025-09-30 13:37:40 +0000

    work

---
+++ conditionalExercise.py
@@ -0,0 +1,25 @@
+def Votecheck(age):
+    if age >= 18:
+        print("You are allowed to vote")
+    else:
+        print("sorry, you are not allowed to vote yet")
+
+def TicketCheck(age):
+    if age <= 13:
+        print("ticket is 10.00")
+    elif age >= 14 and age <25:
+        print("your ticket is 15.00")
+    elif age >= 25 and age <55:
+        print("your ticket is 20.00")
+    else:
+        print("yot ticket is 10.00")
+
+TicketCheck(26)
+
+def maxnumber():
+    a = int(input("please enter your first numer"))
+    b = int(input("please enter your second numer"))
+    c = int(input("please enter your third numer"))
+    hinumber = max(a,b,c)
+
+    print(hinumber)
