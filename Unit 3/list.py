432ec90c1f17d28a8a8c39af53539ab62c5a2d3d
jadencreates11 <jhart184@boyslatin.net>
2025-10-08 13:37:20 +0000

    work

---
+++ Unit 3/list.py
@@ -0,0 +1,25 @@
+#List - a collection system for storing and organizing multiple
+#places of data.
+
+# List syntax (how it is written)
+# when we want to create a list we first create a variable, and then
+# assign it to the square brackets [].
+shoppinglist = ["apples", "oranges", "water", 1,2,3, True, ["bread", "milk"]]
+
+print(shoppinglist)
+
+#if we ant to access an item from a list, we use the index systems.
+# we write thwe list variable name, and then use the brackets and pass in the
+# number postition.
+
+# List are zero based indexed; meaning the lsit count always starts at zero(0)
+
+print(shoppinglist[2])
+trunkparty = ["laptop", "tv", "fridge"]
+
+def addItemForCollage(list):
+    newItem =input("please add new item to your list")
+    list.append(newItem)
+
+    def removeItemFromList(list):
+        item =input("please type what to remove")
