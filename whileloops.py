5eebe52cbfc430de0cab580ee7c788dec453f6e7
jadencreates11 <jhart184@boyslatin.net>
2025-10-16 13:37:10 +0000

    loop

---
+++ whileloops.py
@@ -0,0 +1,24 @@
+
+#while loop - a while loop is a type of construct where code instructions will keep on running so long as a condition is true
+#NOTE - in order to stop a loop (or any program) from running in your terminal, clcik ctrl + c at the same time.
+
+
+ageToBuyGame = 17
+customerage = int(input("how old are you?"))
+
+while ageToBuyGame >= customerage: #17 > 1
+    print("sorry, you are not old enough to buy the game")
+else:
+    print("great, enjoy your game")
+
+
+
+    savedpassword = "1234"
+    usepassword = input("please type in your password")
+    attempt =+1
+    while savedpassword != usepassword:
+        print("incorrect password")
+    if attempt == 3:
+        print("sorry, your account is locked")
+    else:
+        print("welcome to your account")
