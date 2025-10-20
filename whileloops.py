5db18a181bcb16b2256949f63e10802c94b474ee
jadencreates11 <jhart184@boyslatin.net>
2025-10-20 13:17:26 +0000

    Timer

--- whileloops.py
+++ whileloops.py
@@ -3,22 +3,36 @@
 #NOTE - in order to stop a loop (or any program) from running in your terminal, clcik ctrl + c at the same time.
 
 
-ageToBuyGame = 17
-customerage = int(input("how old are you?"))
+#ageToBuyGame = 17
+#customerage = int(input("how old are you?"))
 
-while ageToBuyGame >= customerage: #17 > 1
-    print("sorry, you are not old enough to buy the game")
+#if ageToBuyGame >= customerage: #17 > 1
+    #print("sorry, you are not old enough to buy the game")
+#else:
+    #print("great, enjoy your game")
+
+
+
+   # savedpassword = "1234"
+    #usepassword = input("please type in your password")
+   ## if savedpassword != usepassword:
+   #     print("incorrect password")
+    #if attempt == 3:
+        #print("sorry, your account is locked")
+    #else:
+        #print("welcome to your account")
+    
+
+number = 0
+while number <10:
+    number += 1
+    print(number)
 else:
-    print("great, enjoy your game")
-
-
-
-    savedpassword = "1234"
-    usepassword = input("please type in your password")
-    attempt =+1
-    while savedpassword != usepassword:
-        print("incorrect password")
-    if attempt == 3:
-        print("sorry, your account is locked")
-    else:
-        print("welcome to your account")
+    print("done counting")
+ 
+number = 30
+while number > 0:
+    number -= 1
+    print(number)
+else:
+    print("done counting")
