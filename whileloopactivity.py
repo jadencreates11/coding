c673a30722fd2b473be673029d31a9b1af17c15d
jadencreates11 <jhart184@boyslatin.net>
2025-10-22 13:37:18 +0000

    sum

--- whileloopactivity.py
+++ whileloopactivity.py
@@ -1,11 +1,46 @@
-def numberloop():
-    numberinput = input("please enter a number")
+#numberinput = input("please enter a number")
+    ## print(numberlist)
+     # numberinput = input("please enter number")
+     # print("code is working, please enter numbers")
+   # else:
+      # print("loop has ended")
+      # print("code is working, able to quit")
+
+#i = 1
+#while i < 3:
+   # print(i)
+    #print("beginning of the story")
+    #x = input("type something")
+    #print ("middle of story")
+    #y = input("type something")
+   # print("end of story")
+   # z = input("type something")
+   # i +1
+   # print(i)
+  
+#def numberlistloop():
+    #numberlist = []
+    #usernumber = input("type your number please")
+    #while usernumber != "quit":
+       # newNumber = int(usernumber)
+       # numberlist.append(newNumber)
+       # print(numberlist)
+       # usernumber = input("type in a number")
+   # else:
+        #print("loop has stopped")
+
+#numberlistloop()
+
+def addnumberlist():
     numberlist = []
-    while numberinput != "quit":
-      numberlist.append(numberinput)
-      print(numberlist)
-      numberinput = input("please enter number")
-      print("code is working, please enter numbers")
+    usernumber = input("type your number please")
+    while usernumber != "quit":
+        newNumber = int(usernumber)
+        numberlist.append(newNumber)
+        print(numberlist)
+        usernumber = input("type in a number")
     else:
-       print("loop has ended")
-       print("code is working, able to quit")
+        x = sum(numberlist)
+        print(x)
+
+addnumberlist()
