eaff5c6d22a0c74bb04a9424c3f2c637b9364bf2
jadencreates11 <jhart184@boyslatin.net>
2025-11-11 14:37:05 +0000

    phones

---
+++ Unit 3/PyhtonReview.py
@@ -0,0 +1,32 @@
+# create a new document called pythonReview.py and answer the following
+# questions. 
+
+# Question 1
+# Build a program that determines if a student has submitted their class work 
+# and homework assignment. The program should use an operator that allows 
+# for evaluating 2 conditions and determining if the conditions are true 
+# or false , then answer the following questions: 
+def homework ():
+    if homework == "submitted":
+        print("This Student has submitted there work")
+    else:
+        print("There has been no work submitted")
+
+# Question 2
+# Create a function that will take in a string as an argument and output 
+# that string in reverse order.
+Fruits = ["cheese","meat","apple"]
+Fruits.reverse();
+ptint(Fruits)
+# hint: look into string reverse in w3schools
+
+# Question 3
+# Create a number guessing function where the program will continuously 
+# ask the user to enter a number until the guess the number correctly. 
+# Your program should also give the user information on if their guess 
+# is close to the correct number. If the guess is above the correct number 
+# it should tell the user it is too high and try again. 
+# If the guess is below the number, it should tell the user it is too low, 
+# it should tell them it is too low and to guess again. Once the user gets 
+# it correct the program should congratulate them, stop, and tell them how 
+# many attempts they made.
