40fbc19fc481dbdeb81bcb831a6d77179aff2d71
jadencreates11 <jhart184@boyslatin.net>
2025-11-18 14:37:35 +0000

    r,p,s

---
+++ rps.py
@@ -0,0 +1,3 @@
+"I will make a function that takes a random word, either rock, paper, or scissors and then the user will type out a word and you will either get a draw, win, or lose"
+
+"It will be able to compare, it will take numbers and ccomapre them, we can also loop"
---
+++ sdlc.py
@@ -0,0 +1,19 @@
+"It should be able to be able to do addition, subtraction, division, and multiplication"
+
+"I would use a could that will take any integers and it will either add them together, subtract them, divide them, or multiply them and give me sum"
+
+def add():
+    num1 = int(input("please type in a number"))
+    num2 = int(input("please type in another number"))
+    sum = num1 + num2
+    print("sum =" + str(sum))
+    bonusNum = int(input("would you like to add another number to the sum"))
+    while bonusNum != "yes":
+        if bonusNum == "y":
+          numX = int(input("please type in number"))
+          print(sum + numX)
+        def add (sum):
+           print
+
+  
+  
