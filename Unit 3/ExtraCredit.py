3ebd5d6e2c14aeddfd11f5334043e2e8070e81b6
jadencreates11 <jhart184@boyslatin.net>
2025-11-25 14:37:18 +0000

    Extra Credit

---
+++ Unit 3/ExtraCredit.py
@@ -0,0 +1,23 @@
+#def gradecheck(grade):
+   #if grade >=90:
+      #print("your grade is an A")
+  #elif grade >=80:
+      #print("you have a B")
+  # elif grade >=70:
+     # print("your grade is a C")
+  # else:
+      #print("you have an F")
+
+#gradecheck(69)
+
+#Kg = 0
+#while Kg < 100:
+   #Kg += 2.2
+   #print(Kg)
+
+def removeitemfromlist(list):
+    Jadens_backpack = ["snacks", "soda", "binder", "pens", "notebook"]
+    item = input("please type what item you want to remove")
+    Jadens_backpack.remove(item)
+    print (Jadens_backpack)
+
