2d20349b93bf3ab4f6c875feeb40aee2720d1f3d
jadencreates11 <jhart184@boyslatin.net>
2025-12-12 14:37:31 +0000

    GPA

--- Projects/miniProject2.py
+++ Projects/miniProject2.py
@@ -4,15 +4,28 @@ def Calculator():
     subject = input()
     print(" This Subject is" + subject)
     gradelist = []
-    week = 1
+    week = 1 
     print("What is the grade number for this week" + str(week))
-    gradenumber = input("type your grade number please")
-    while gradenumber != "OverAll":
-        grade = int(gradenumber)
-        gradelist.append(grade)
-        print(gradelist)
-        gradenumber = input("type in a number")
+    gradenumber = input("WHat is the grade for this week")
+    while gradenumber != "Overall":
+         grade = int(gradenumber)
+         gradelist.append(grade)
+         print(gradelist)
+         gradenumber = input("type in the grade for next week")
     else:
-        x = sum(gradelist)
-        print("This is the overall grade" + x / 17)
+          x = sum(gradelist)
+          print(x / 17)
+    if grade >=90:
+      print("your grade is an A")
+    elif grade >=80:
+      print("you have a B")
+    elif grade >=70:
+      print("your grade is a C")
+    elif grade >= 60:
+       print("you have a D")
+    else:
+      print("you have an F")
+
+
+
 Calculator()
