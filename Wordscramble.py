dedc08675823f32bd4f4f3a20a603657a0c7e938
jadencreates11 <jhart184@boyslatin.net>
2026-01-09 14:37:22 +0000

    Scramble

--- Wordscramble.py
+++ Wordscramble.py
@@ -1,9 +1,7 @@
 import random
-
-
 def scramblewordgame():
     point = 0
-    wordpool = ["Philadelpha", "North Carolina", "congregate", "funtion"]
+    wordpool = ["Philadelphia", "North Carolina", "congregate", "funtion"]
     print("welcome to word scramble")
     randomwordselect = random.randint(0,3)
     correctword = ""
@@ -13,23 +11,41 @@ def scramblewordgame():
     elif randomwordselect == 1:
         print(wordpool[1])
         correctword = wordpool[1]
+    elif userGuess == correctword:
+        point += 1
+        print("this is correct")
     elif randomwordselect == 2:
         print(wordpool[2])
         correctword = wordpool[2]
+    elif userGuess == correctword:
+        point += 1
+        print("this is correct")
     elif randomwordselect == 3:
         print(wordpool[3])
         correctword = wordpool[3]
+    elif userGuess == correctword:
+        point += 1
+        print("this is correct")
+    else:
+        print("this is incorrect")
 
     convertedselection = list(correctword)
     random.shuffle(convertedselection)
     scrambled = "".join(convertedselection)
+    
     print("guess the correct word" + scrambled)
     userGuess = input()
     if userGuess == correctword:
-        print("this is correct")
+        point +=1
+        print("correct")
+        print("Here is your score:" + str(point)+ "/3")
+        print(point)
+    
     else:
-        print("this is wrong")
-
+     print("incorrect")
+     print("Here is your score:" + str(point)+ "/3")
+     print(point)
+    
     print(convertedselection)
 
 scramblewordgame()
