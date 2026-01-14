eac16e453062d4a0e7790361a5f475878336c076
jadencreates11 <jhart184@boyslatin.net>
2026-01-14 14:37:16 +0000

    Car

--- BankAppCode.py
+++ BankAppCode.py
@@ -14,4 +14,21 @@ def payCheckFilter(payrate, hours, daysworked):
     print ("Retirment account balance" + str(retirementaccount))
     print("checking account balance" + str(checkingaccount))
 
-payCheckFilter(50.00, 8, 300)
+#payCheckFilter(50.00, 8, 300)
+
+
+def ridesharecalculator(miles, surgeprice, discount):
+    Basefare = 3.00
+    coastpermile = 2.00
+    surgeprice = 3.75
+    total = Basefare + miles * coastpermile
+    if surgeprice == True:
+        print("The final price for this ride is " + str(surgeprice +(miles * coastpermile)))
+    else:
+        print("The final price for this ride is " + str(Basefare +(miles * coastpermile)))
+        if discount == True:
+            discountprice = total * .35
+            print("The final price for this ride is" + str(surgeprice))
+    
+
+ridesharecalculator(10, False)
